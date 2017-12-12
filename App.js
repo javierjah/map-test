5719d69815ffa2f55b9eca5d94875c8c06a07d78
javierjah <javier.palacios.h@gmail.com>
2017-12-11 22:37:31 -0300

    Improve marker and set zoom in locations

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button, Alert } from 'react-native';
+import { View, StyleSheet, Button, Alert, FlatList, Text, TouchableHighlight } from 'react-native';
 import { Constants, MapView, Location, Permissions } from 'expo';
+import customMarker from './assets/marker.png'
 
 export default class App extends Component {
   state = {
-    mapRegion: { latitude: 37.78825, longitude: -122.4324},
-    locationResult: null
+    mapRegion: {
+      latitude: -33.454402,
+      longitude: -70.560250,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    },
+    locationResult: null,
+    corsList: [],
   };
 
   componentDidMount() {
+    fetch('http://maps-api-dev.us-west-2.elasticbeanstalk.com/api/maps')
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+        return false
+      })
+      .then((res) => {
+        const corsList = res.map((map) => {
+          map.cords[0].latitudeDelta = 0.0922
+          map.cords[0].longitudeDelta = 0.0922
+          return map;
+        })
+
+        this.setState({corsList})
+      })
+      .catch((err) => console.log(err))
+
     this._getLocationAsync();
   }
 
@@ -41,27 +66,39 @@ export default class App extends Component {
    });
  };
 
+ _onPress (region) {
+  this.setState({mapRegion: region.cords[0]})
+ }
+
   render() {
     return (
       <View style={styles.container}>
 
         <MapView
-          style={{ alignSelf: 'stretch', height: '80%'}}
+          style={{ alignSelf: 'stretch', height: '50%'}}
           region={this.state.mapRegion}
           onRegionChange={this._handleMapRegionChange}
         >
           <MapView.Marker
             coordinate={this.state.mapRegion}
+            image={customMarker}
             title={'Tu ubicación'}
             description={'descripcion ubicación'}
           />
         </MapView>
 
-        <Button
-          title="Aceptar"
-          onPress={this._handleButtonPress}
-        />
-
+        <View style={{ alignSelf: 'stretch', height: '50%'}}>
+          <View style={styles.container}>
+            <FlatList
+              data={this.state.corsList}
+              renderItem={({item}) => <TouchableHighlight key={item.id} onPress={() => this._onPress(item)} underlayColor='rgba(50, 69, 88, 0.8)'><Text style={styles.item}>{item.name}</Text></TouchableHighlight>}
+            />
+          </View>
+          <Button
+            title="Aceptar"
+            onPress={this._handleButtonPress}
+          />
+        </View>
       </View>
     );
   }
@@ -75,4 +112,14 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
   },
+  container: {
+    flex: 1,
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
+
